8292e05ed20ac68db15e2383d14b39f5e2543177
Jesse Greenough <jesse.greenough84@gmail.com>
2025-02-19 00:29:45 -0800

    Update module structure for transports directory - re-exported transport base properties from transport sub module

--- engine/src/transports/mod.rs
+++ engine/src/transports/mod.rs
@@ -1,16 +1,29 @@
-pub(crate) mod transport;
-pub(crate) mod polling;
-pub(crate) mod polling_json;
-pub(crate) mod web_socket;
-pub(crate) mod web_transport;
+mod transport;
+
+pub mod polling;
+pub mod web_socket;
+pub mod web_transport;
 
 use std::ops::Deref;
 
+pub use transport::{
+    Transport,
+    TransportBase,
+    ReadyState,
+    EngineRequest,
+    EngineResponse,
+    EngineResult,
+};
+
+
+#[derive(Clone, Debug, Default)]
 pub enum TransportType {
+    #[default]
     Polling,
     WebSocket,
     WebTransport
 }
+
 pub fn polling_upgrades_to() -> Vec<TransportType> {
     vec![TransportType::WebSocket, TransportType::WebTransport]
 }
