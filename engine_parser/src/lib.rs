4c66e28bbeb7d9d79d9bf1d460c1f51fb5d91f79
Jesse Greenough <jesse.greenough84@gmail.com>
2025-02-21 23:59:52 -0800

    Remove Encode and Decode Trait declarations (Opt for modularzed Packet implementations)

--- engine_parser/src/lib.rs
+++ engine_parser/src/lib.rs
@@ -4,15 +4,12 @@ mod event_emitter;
 
 #[cfg(test)]
 mod tests;
-mod transport;
 
 pub use packet::{
     Packet,
     PacketType,
     PacketOptions,
-    encode::Encode,
     encoding_stream::PacketEncoderStream,
-    decode::Decode,
     decoding_stream::PacketDecoderStream,
 };
 pub use constants::{
