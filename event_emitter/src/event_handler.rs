f363092f0fb437c22d1b22096385b353516d6ad3
Jesse Greenough <jesse.greenough84@gmail.com>
2025-02-24 23:56:16 -0800

    Finalize EventHAndler trait structure, method naming, and method signatures

--- event_emitter/src/event_handler.rs
+++ event_emitter/src/event_handler.rs
@@ -1,40 +1,39 @@
-use std::pin::Pin;
-use crate::{EventError, EventPayload};
+use std::future::Future;
+use crate::{Callback, EventError, EventPayload};
 use crate::listener::Listener;
 
 /// A generic trait with functionality required for any struct that intends to react to events in accordance to this package
-pub trait EventHandler<T: Send + Sync + Clone>: Send + Sync + Clone {
+pub trait EventHandler<T: Send + Sync>: Send + Sync {
     fn event_names(&self) -> Vec<String>;
+    fn set_max_listeners(&mut self, max: usize);
+    fn max_listeners(&self) -> usize;
 
-    /// Infinite Listener
-    fn add_listener(&mut self, event_name: &str, callback: Listener<T>) -> Result<(), EventError>;
-    fn on(&mut self, event_name: &str, callback: Listener<T>) -> Result<(), EventError>;
-    /// Finite Listener
-    fn add_limited_listener(&mut self, event_name: &str, callback: Listener<T>) -> Result<(), EventError>;
-    fn on_limited(&mut self, event_name: &str, callback: Listener<T>, limit: u64) -> Result<(), EventError>;
-    /// Single Instance Listener
-    fn once(&mut self, event_name: &str, callback: Listener<T>) -> Result<(), EventError>;
+    /// Add an infinite Listener
+    fn add_listener(&mut self, event_name: &str, callback: Callback<T>) -> Result<(), EventError>;
+    /// Add a finite Listener
+    fn add_limited_listener(&mut self, event_name: &str, callback: Callback<T>, limit: u64) -> Result<(), EventError>;
+    /// Add a single instance Listener
+    fn add_once(&mut self, event_name: &str, callback: Callback<T>) -> Result<(), EventError>;
+
+    /// Check the number of listeners that are registered to a certain event<br/>
+    /// Throws an error for event names that have no active listeners (are not currently registered)
+    fn listener_count(&self, event_name: &str) ->  usize;
+    /// Check if an event has any registered listeners
+    fn has_listener(&self, event_name: &str) ->  bool {
+        self.listener_count(event_name) > 0
+    }
+
     /// Remove an active listener for an Event
     fn remove_listener(&mut self, event_name: &str, callback: &Listener<T>) -> Result<(), EventError>;
-    fn off(&mut self, event_name: &str, callback: &Listener<T>) -> Result<(), EventError>;
     /// Remove all Listeners for an Event
     fn remove_all_listeners(&mut self, event_name: &str) -> Result<(), EventError>;
 
-    fn emit(&mut self, event_name: &str, payload: EventPayload<T>);
-    async fn emit_async(&mut self, event_name: &str, payload: EventPayload<T>);
-
-    fn emit_final(&mut self, event_name: &str, payload: EventPayload<T>) -> Result<(), EventError>{
-        self.emit(event_name, payload);
-        self.remove_all_listeners(event_name)
-    }
-    fn emit_final_async(&mut self, event_name: &str, payload: EventPayload<T>) -> Pin<Box<dyn futures::Future<Output = Result<(), EventError>>>> {
-        Box::pin(async move {
-            self.emit_async(event_name, payload).await;
-            self.remove_all_listeners(event_name)
-        })
-    }
-
-    fn set_max_listeners(&mut self, max: usize);
-    fn max_listeners(&self) -> usize;
-    fn listener_count(&self, event_name: &str) -> Result<usize, EventError>;
+    /// Ensure finality/success of all event callbacks
+    fn emit(&mut self, event_name: &str, payload: EventPayload<T>) -> Result<(), EventError>;
+    /// Ensure finality/success of all event callbacks before deleting the event
+    fn emit_final(&mut self, event_name: &str, payload: EventPayload<T>) -> Result<(), EventError>;
+    /// Fire and forget all event callbacks
+    fn emit_async(&mut self, event_name: &str, payload: EventPayload<T>) -> Box<dyn Future<Output=()> + Send>;
+    /// Fire and forget all event callbacks before deleting the event
+    fn emit_final_async(&mut self, event_name: &str, payload: EventPayload<T>) -> Box<dyn Future<Output=()> + Send>;
 }
