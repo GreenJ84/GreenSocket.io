55deec323cacebd5c5282b4c33e547ed66afe635
Jesse Greenough <jesse.greenough84@gmail.com>
2025-02-25 22:28:25 -0800

    Update listener module - Remove breaking Debug trait derivation (need to custom implement debug) - Implement equality and expiration methodology

--- event_emitter/src/listener.rs
+++ event_emitter/src/listener.rs
@@ -3,20 +3,26 @@ use std::sync::Arc;
 use crate::{Callback, EventPayload};
 
 /// A Struct that is used to enact upon an emit Event
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Listener<T> {
     callback: Callback<T>,
     lifetime: Option<u64>,
 }
 impl<T> Listener<T> {
-    pub fn new<T>(callback: Arc<Callback<T>>, lifetime: Option<u64>) -> Self {
+    pub fn new(callback: Callback<T>, lifetime: Option<u64>) -> Self {
         Self { callback, lifetime }
     }
-    pub fn call<T>(&self, payload: EventPayload<T>){
-        self.callback(payload);
+    pub fn call(&self, payload: &EventPayload<T>) {
+       (self.callback)(payload);
         if let Some(mut lifetime) = self.lifetime{
-            *lifetime -= 1;
+            lifetime -= 1;
         }
+    }
+    pub fn at_limit(&self) -> bool {
+        self.lifetime == Some(0)
+    }
+    pub fn eq_callback(&self, callback: Callback<T>) -> bool{
+        Arc::ptr_eq(&self.callback, &callback)
     }
 }
 
