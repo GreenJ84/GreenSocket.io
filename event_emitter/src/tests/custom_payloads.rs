a22e7e284d0d35aa4ffac84eed983e9cd10d7506
Jesse Greenough <jesse.greenough84@gmail.com>
2025-03-01 22:56:42 -0800

    Create tests validating the emission of custom struct data payloads

--- event_emitter/src/tests/custom_payloads.rs
+++ event_emitter/src/tests/custom_payloads.rs
@@ -181,3 +181,77 @@ mod binary_payload {
         assert_eq!(*count.lock().unwrap(), 10, "Async event callbacks unsuccessful");
     }
 }
+
+mod custom_payload {
+    use super::*;
+
+    #[derive(PartialEq, Eq, Debug)]
+    struct TestCustomPayload {
+        message: String,
+        option: bool,
+        val: u32
+    }
+    fn test_custom_payload(data: &str, val: u32) -> EventPayload<TestCustomPayload> {
+        Arc::new(TestCustomPayload {
+            message: data.to_string(),
+            option: val % 2 == 0 && val % 4 == 0,
+            val,
+        })
+    }
+
+    #[test]
+    fn custom_emit_successful() {
+        let mut emitter = EventManager::<TestCustomPayload>::new(EventEmitter::default());
+        let count = Arc::new(Mutex::new(0));
+        let count_clone = Arc::clone(&count);
+        {
+            let emitter = Arc::get_mut(&mut emitter).unwrap();
+            assert!(
+                emitter.add_listener("count", Arc::new(move |payload| {
+                    let received = payload.as_ref();
+                    assert_eq!(received.message, String::from("custom"));
+                    assert_eq!(received.option, received.val % 2 == 0 && received.val % 4 == 0);
+                    *count_clone.lock().unwrap() += 1;
+                })).is_ok(),
+                "Failed to add event listener"
+            );
+
+            for val in 0..10 {
+                assert!(
+                    emitter.emit("count", test_custom_payload("custom", val as u32)).is_ok(),
+                    "Failed to emit event"
+                );
+            }
+        }
+
+        assert_eq!(*count.lock().unwrap(), 10, "Event callbacks unsuccessful");
+    }
+
+
+    #[tokio::test]
+    async fn custom_async_emission_successful() {
+        let mut emitter = EventManager::<TestCustomPayload>::new(EventEmitter::default());
+        let count = Arc::new(Mutex::new(0));
+        let count_clone = Arc::clone(&count);
+        {
+            let emitter = Arc::get_mut(&mut emitter).unwrap();
+            assert!(
+                emitter.add_listener("async_event", Arc::new(move |payload| {
+                    let received = payload.as_ref();
+                    assert_eq!(received.message, String::from("custom_async"));
+                    *count_clone.lock().unwrap() += 1;
+                })).is_ok(),
+                "Failed to add event listener"
+            );
+
+            for val in 0..10 {
+                assert!(
+                    emitter.emit_async("async_event", test_custom_payload("custom_async", val as u32), false).is_ok()
+                );
+                sleep(Duration::from_millis(100)).await;
+            }
+        }
+
+        assert_eq!(*count.lock().unwrap(), 10, "Async event callbacks unsuccessful");
+    }
+}
