18ef018c75b0e7521c6069d5b0e289bdd368a032
Jesse Greenough <jesse.greenough84@gmail.com>
2025-03-03 01:02:26 -0800

    Add Documentation to the Listener struct and implemented methods.

--- event_emitter/src/listener.rs
+++ event_emitter/src/listener.rs
@@ -9,6 +9,21 @@ pub struct Listener<T> {
     lifetime: Option<Arc<Mutex<u64>>>,
 }
 impl<T: Send + Sync + 'static> Listener<T> {
+    /// Constructs a new `Listener<T>` from a given a `Callback<T>` and lifetime `Option<u64>`
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use std::sync::Arc;
+    /// use event_emitter::{Callback, EventPayload, Listener};
+    ///
+    /// let listener = Listener::new(
+    ///     Arc::new(move |payload: &EventPayload<String>| {
+    ///         // Handle payload
+    ///     }),
+    ///     Some(10)
+    /// );
+    /// ```
     pub fn new(callback: Callback<T>, lifetime: Option<u64>) -> Self {
         if let Some(limit) = lifetime {
             return Self { callback, lifetime: Some(Arc::new(Mutex::new(limit))) };
@@ -16,7 +31,19 @@ impl<T: Send + Sync + 'static> Listener<T> {
         Self { callback, lifetime: None }
     }
 
-    /// Best for Synchronous order dependant tasks
+    /// Synchronously run the callback for a `Listener<T>` with an emitted event payload. <br/>
+    /// If the listener has a lifetime, it will be decreased by 1 for the current emission.
+    ///
+    /// Best for Synchronous order dependant tasks.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use event_emitter::{Callback, EventPayload, Listener};
+    ///
+    /// let mut listener = Listener::default();
+    /// listener.call(&EventPayload::new(String::new()));
+    /// ```
     pub fn call(&mut self, payload: &EventPayload<T>) {
         if let Some(ref lifetime) = self.lifetime {
             let mut count = lifetime.lock().unwrap();
@@ -25,7 +52,21 @@ impl<T: Send + Sync + 'static> Listener<T> {
         (self.callback)(payload);
     }
 
+    /// Asynchronously run the callback for a `Listener<T>` ana emitted event payload within a concurrent task pool. <br/>
+    /// If the listener has a lifetime, it will be decreased by 1 for the current emission.
+    ///
     /// Best for Background Processing and I/O heavy tasks
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use event_emitter::{Callback, EventPayload, Listener};
+    ///
+    /// let mut listener = Listener::default();
+    /// # tokio_test::block_on(async {
+    /// listener.background_call(&EventPayload::new(String::new()));
+    /// # })
+    /// ```
     pub fn background_call(&mut self, payload: &EventPayload<T>) {
         if let Some(ref lifetime) = self.lifetime {
             let mut count = lifetime.lock().unwrap();
@@ -41,7 +82,21 @@ impl<T: Send + Sync + 'static> Listener<T> {
         });
     }
 
+    /// Asynchronously run the callback for a `Listener<T>` ana emitted event payload within a blocking thread pool. <br/>
+    /// If the listener has a lifetime, it will be decreased by 1 for the current emission.
+    ///
     /// Best for CPU heavy Operations
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use event_emitter::{EventPayload, Listener};
+    ///
+    /// let mut listener = Listener::default();
+    /// # tokio_test::block_on(async {
+    /// listener.blocking_call(&EventPayload::new(String::new()));
+    /// # })
+    /// ```
     pub fn blocking_call(&mut self, payload: &EventPayload<T>) {
         if let Some(ref lifetime) = self.lifetime {
             let mut count = lifetime.lock().unwrap();
@@ -55,6 +110,7 @@ impl<T: Send + Sync + 'static> Listener<T> {
         });
     }
 
+    /// Check whether the `Listener<T>` has fulfilled its lifetime.
     pub fn at_limit(&self) -> bool {
         if let Some(ref lifetime) = self.lifetime {
             let count = lifetime.lock().unwrap();
@@ -62,11 +118,13 @@ impl<T: Send + Sync + 'static> Listener<T> {
         }
         false
     }
+
+    /// Check whether a `Callback<T>` is the same Arc reference as in the `Listener<T>`.
     pub fn eq_callback(&self, callback: Callback<T>) -> bool{
         Arc::ptr_eq(&self.callback, &callback)
     }
 }
-impl<T> Clone for Listener<T> {
+impl<T:  Send + Sync + 'static> Clone for Listener<T> {
     fn clone(&self) -> Self {
         Self {
             callback: Arc::clone(&self.callback),
@@ -81,6 +139,21 @@ impl<T> Clone for Listener<T> {
         self.lifetime = if let Some(limit) = &source.lifetime {
             Some(Arc::clone(limit))
         } else { None };
+    }
+}
+impl<T: Send + Sync + 'static> Default for Listener<T> {
+    /// Returns the default value for the `Listener<T>` struct
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use event_emitter::EventEmitter;
+    ///
+    /// // Type annotation required
+    /// let emitter = EventEmitter::<String>::default();
+    /// ```
+    fn default() -> Self {
+        Self::new(Arc::new(move |_: &EventPayload<T>| { println!("You wasted an Event, congrats!") }), Some(1))
     }
 }
 impl<T> Debug for Listener<T> {
