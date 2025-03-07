25526c93a85e1cc56aa8c7ac874df220e9808751
Jesse Greenough <jesse.greenough84@gmail.com>
2025-03-07 09:31:10 -0800

    Bug Fix: Remove breaking RawData inner method - Accessing inner allocated enum data has to be manually matched when needed

--- engine_parser/src/constants.rs
+++ engine_parser/src/constants.rs
@@ -14,12 +14,6 @@ pub enum RawData {
     Binary(BinaryType)
 }
 impl RawData {
-    pub fn inner<T>(self) -> T {
-        match self {
-            RawData::Binary(b) => b,
-            RawData::Text(s) => s,
-        }
-    }
     pub fn len(&self) -> usize {
         match self {
             RawData::Binary(b) => b.len(),
