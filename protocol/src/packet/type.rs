673fa2ae7f3d31c359dd0a6023b82393943f3c2c
Jesse Greenough <jesse.greenough84@gmail.com>
2025-09-11 12:34:55 -0700

    Separate PacketType enum from Packet structure - Implement TryFrom trait

---
+++ protocol/src/packet/type.rs
@@ -0,0 +1,89 @@
+use std::convert::TryFrom;
+
+/// Represents the type of packet.
+/// Each variant corresponds to a specific packet type in the protocol.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PacketType {
+    /// Open Connection.
+    Open = 0,
+    /// Close Connection.
+    Close = 1,
+    /// Ping packet. (Heartbeat implementation)
+    Ping = 2,
+    /// Pong packet. (Heartbeat implementation)
+    Pong = 3,
+    /// Message packet.
+    Message = 4,
+    /// Transport upgrade packet.
+    Upgrade = 5,
+    /// No-operation packet.
+    Noop = 6,
+    /// Error packet.
+    Error = -1,
+}
+
+impl TryFrom<&str> for PacketType {
+  type Error = ();
+
+  fn try_from(s: &str) -> Result<Self, Self::Error> {
+    match s {
+      "open" => Ok(Self::Open),
+      "close" => Ok(Self::Close),
+      "ping" => Ok(Self::Ping),
+      "pong" => Ok(Self::Pong),
+      "message" => Ok(Self::Message),
+      "upgrade" => Ok(Self::Upgrade),
+      "noop" => Ok(Self::Noop),
+      "error" => Ok(Self::Error),
+      _ => Err(()),
+    }
+  }
+}
+
+impl TryFrom<i8> for PacketType {
+  type Error = ();
+
+  fn try_from(c: i8) -> Result<Self, Self::Error> {
+    match c {
+      0 => Ok(Self::Open),
+      1 => Ok(Self::Close),
+      2 => Ok(Self::Ping),
+      3 => Ok(Self::Pong),
+      4 => Ok(Self::Message),
+      5 => Ok(Self::Upgrade),
+      6 => Ok(Self::Noop),
+      -1 => Ok(Self::Error),
+      _ => Err(()),
+    }
+  }
+}
+
+impl PacketType {
+  /// Returns the string representation of the packet type.
+  pub fn as_str(&self) -> &'static str {
+    match self {
+      Self::Open => "open",
+      Self::Close => "close",
+      Self::Ping => "ping",
+      Self::Pong => "pong",
+      Self::Message => "message",
+      Self::Upgrade => "upgrade",
+      Self::Noop => "noop",
+      Self::Error => "error",
+    }
+  }
+
+  /// Returns the integer representation of the packet type.
+  pub fn as_int(&self) -> i8 {
+    match self {
+      Self::Open => 0,
+      Self::Close => 1,
+      Self::Ping => 2,
+      Self::Pong => 3,
+      Self::Message => 4,
+      Self::Upgrade => 5,
+      Self::Noop => 6,
+      Self::Error => -1,
+    }
+  }
+}
