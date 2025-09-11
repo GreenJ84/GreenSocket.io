fdbbb31b43739e697985d243a2a117a755913cd0
Jesse Greenough <jesse.greenough84@gmail.com>
2025-09-10 18:43:13 -0700

    - Update packet documentation - Add integer (i8) conversions of PacketType

--- protocol/src/packet/mod.rs
+++ protocol/src/packet/mod.rs
@@ -5,19 +5,30 @@ pub(crate) mod decoding_stream;
 
 use crate::constants::RawData;
 
-
+/// Represents the type of packet.
+/// Each variant corresponds to a specific packet type in the protocol.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PacketType {
+    /// Open Connection.
     Open = 0,
+    /// Close Connection.
     Close = 1,
+    /// Ping packet. (Heartbeat implementation)
     Ping = 2,
+    /// Pong packet. (Heartbeat implementation)
     Pong = 3,
+    /// Message packet.
     Message = 4,
+    /// Transport upgrade packet.
     Upgrade = 5,
+    /// No-operation packet.
     Noop = 6,
+    /// Error packet.
     Error = -1,
 }
+
 impl PacketType {
+    /// Converts a string to a `PacketType`, if possible.
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
             "open" => Some(Self::Open),
@@ -31,6 +42,8 @@ impl PacketType {
             _ => None,
         }
     }
+
+    /// Converts a character to a `PacketType`, if possible.
     pub fn from_char(c: char) -> Option<Self> {
         match c {
             '0' => Some(Self::Open),
@@ -40,10 +53,27 @@ impl PacketType {
             '4' => Some(Self::Message),
             '5' => Some(Self::Upgrade),
             '6' => Some(Self::Noop),
-            'e' => Some(Self::Error), // Error mapped to "-1" in your previous implementation
+            'e' => Some(Self::Error), // Error mapped to "-1"
             _ => None,
         }
     }
+
+    /// Converts a integer to a `PacketType`, if possible.
+    pub fn from_int(c: i8) -> Option<Self> {
+        match c {
+            0 => Some(Self::Open),
+            1 => Some(Self::Close),
+            2 => Some(Self::Ping),
+            3 => Some(Self::Pong),
+            4 => Some(Self::Message),
+            5 => Some(Self::Upgrade),
+            6 => Some(Self::Noop),
+            -1 => Some(Self::Error),
+            _ => None,
+        }
+    }
+
+    /// Returns the string representation of the packet type.
     pub fn as_str(&self) -> &'static str {
         match self {
             Self::Open => "open",
@@ -56,6 +86,8 @@ impl PacketType {
             Self::Error => "error",
         }
     }
+
+    /// Returns the character representation of the packet type.
     pub fn as_char(&self) -> char {
         match self {
             Self::Open => '0',
@@ -68,10 +100,23 @@ impl PacketType {
             Self::Error => 'e',
         }
     }
+
+    /// Returns the integer representation of the packet type.
+    pub fn as_int(&self) -> i8 {
+        match self {
+            Self::Open => 0,
+            Self::Close => 1,
+            Self::Ping => 2,
+            Self::Pong => 3,
+            Self::Message => 4,
+            Self::Upgrade => 5,
+            Self::Noop => 6,
+            Self::Error => -1,
+        }
+    }
 }
 
-
-
+/// Maximum allowed packet size (1 MB).
 pub const MAX_PACKET_SIZE: usize = 1024 * 1024;
 
 /// Error type for packet creation and validation.
@@ -81,15 +126,31 @@ pub enum PacketError {
     DataTooLarge,
 }
 
+
+
+/// Represents a protocol packet, including its type, options, and data.
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct Packet {
+    /// The type of the packet.
     pub _type: PacketType,
+    /// Optional transmission options.
     pub options: Option<PacketOptions>,
+    /// Optional packet data (text or binary).
     pub data: Option<RawData>,
 }
+
 impl Packet {
-    /// Creates a new packet with validation.
-    /// Returns an error if the data exceeds MAX_PACKET_SIZE.
+    /// Creates a new packet.
+    ///
+    /// # Arguments
+    ///
+    /// * `_type` - The type of the packet.
+    /// * `options` - Optional transmission options.
+    /// * `data` - Optional packet data (text or binary).
+    ///
+    /// # Errors
+    ///
+    ///! Returns `PacketError::DataTooLarge` if the data exceeds the maximum allowed size.
     pub fn new(
         _type: PacketType,
         options: Option<PacketOptions>,
@@ -103,6 +164,11 @@ impl Packet {
         Ok(Self { _type, options, data })
     }
 
+    /// Creates an error packet with the given message.
+    ///
+    /// # Arguments
+    ///
+    /// * `message` - The error message to include in the packet.
     pub fn error(message: &str) -> Self {
         Self {
             _type: PacketType::Error,
@@ -114,9 +180,11 @@ impl Packet {
 
 
 
+/// Options for packet transmission, such as compression.
 // #[cfg(not(feature = "sequencing"))]
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct PacketOptions {
+    /// Whether the packet should be compressed.
     pub compress: bool,
 }
 
@@ -126,4 +194,4 @@ pub struct PacketOptions {
 //     pub compress: bool,
 //     pub sequence: Option<usize>,
 //     pub total_chunks: Option<usize>,
-// }
+//
