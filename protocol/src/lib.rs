cbbc85fc5693b15d68556d5e8a2b62ef913286b1
Jesse Greenough <jesse.greenough84@gmail.com>
2025-11-12 05:43:02 -0800

    Remove  parser module - Unnecessary wrapper for transitioned encoding/decoding submodule

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,23 +1,18 @@
 mod constants;
+mod decoding;
+mod encoding;
+mod error;
 mod packet;
-mod parser;
 
 #[cfg(test)]
 mod tests;
 
+pub use error::{DecodingError, EncodingError};
 pub use packet::{
-    Packet,
-    MAX_PACKET_SIZE,
-    types::PacketType,
-    options::PacketOptions,
-    error::PacketError,
+    error::PacketError, options::PacketOptions, types::PacketType, Packet, MAX_PACKET_SIZE,
 };
-pub use parser::{
-    error::{DecodingError, EncodingError},
-//     encoding_stream::PacketEncoderStream,
-//     decoding_stream::PacketDecoderStream,
-};
-pub use constants::{
-    BinaryType,
-    RawData
-};
+
+// encoding_stream::PacketEncoderStream,
+// decoding_stream::PacketDecoderStream,
+
+pub use constants::{BinaryType, RawData};
--- protocol/src/parser/mod.rs
+++
@@ -1,3 +0,0 @@
-pub(crate) mod encoding;
-pub(crate) mod decoding;
-pub(crate) mod error;
